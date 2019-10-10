030641f7d8fbe77749d7629e1455c9b1f185acec
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-10-10 17:22:45 +1100

    Add files via upload

---
+++ import_db.sh
@@ -0,0 +1,10 @@
+wget -N -P /home http://web.acma.gov.au/rrl-updates/spectra_rrl.zip
+apt-get update --fix-missing
+apt-get install -y zip
+cd /home && unzip -o spectra_rrl.zip
+export PGPASSWORD=Venus2019
+wget -N -P /home https://raw.githubusercontent.com/robin0821/venus/master/acma_import.sql
+psql -h localhost -d venus -p 5432 -U dataraft -w -a -f /home/acma_import.sql
+
+
+
