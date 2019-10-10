f625dfcad41b7ca4e6848caceb84c458687bdab9
Robin Du <31956921+robin0821@users.noreply.github.com>
2019-10-10 17:23:09 +1100

    Update import_db.sh

--- import_db.sh
+++ import_db.sh
@@ -2,7 +2,7 @@ wget -N -P /home http://web.acma.gov.au/rrl-updates/spectra_rrl.zip
 apt-get update --fix-missing
 apt-get install -y zip
 cd /home && unzip -o spectra_rrl.zip
-export PGPASSWORD=Venus2019
+export PGPASSWORD=Password1234
 wget -N -P /home https://raw.githubusercontent.com/robin0821/venus/master/acma_import.sql
 psql -h localhost -d venus -p 5432 -U dataraft -w -a -f /home/acma_import.sql
 
